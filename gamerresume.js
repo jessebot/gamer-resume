c7b905028a0e78b77b80d0778ee88a1d362cc307
jessebot <jessebot@linux.com>
2017-06-25 04:07:29 +0000

    lolz - this is just all sorts of broken- BUT WE'RE GETTING SOMEWHERE

--- gamerresume.js
+++ gamerresume.js
@@ -10,41 +10,34 @@ var ejs = require('ejs');
 var Steam = require('steam-webapi');
 
 
-function getSteamId() {
-  // Retrieve the steam ID from a steam username/communityID
-  var json = JSON.parse(fs.readFile('./config/config.json'));
-
-  custom_vanity = json.social_handles.steam_vanity_ID;
-  api_key = json.steam_api_key;
-  // Set global Steam API Key
-  Steam.key = api_key;
-  // grab custom steam data
-  Steam.ready(function(err) {
-    if (err) return console.log(err);
-    var steam = new Steam();
-    steam.resolveVanityURL({vanityurl:custom_vanity}, function(err, data) {
-      // data -> { steamid: '76561197968620915', success: 1 }
-      json.social_handles.steam_id = data.steamid;
-      console.log(json.social_handles.steam_id);
-      var converted_json = JSON.stringify(json, null, 2); // spacing level = 2?
-      fs.writeFile('./config/config.json', converted_json, 'utf8'); // write it back
-    });
-  });
-};
-
-
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
 router.get("/",function(req,res){
-  getSteamId();
-  var json = JSON.parse(fs.readFile('./config/config.json', 'utf8'));
-
-  // reload json and make sure we got that handle....
-  console.log("this is after the new functions");
-  console.log(json.social_handles.steam_id);
+  var json;
+  fs.readFile('./config/config.json', 'utf8', function (err, data) {
+      if (err) throw err;
+        json = JSON.parse(data);
+        // Retrieve the steam ID from a steam username/communityID
+        custom_vanity = json.social_handles.steam_vanity_URL;
+        api_key = json.steam_api_key;
+        // Set global Steam API Key
+        Steam.key = api_key;
+        // grab custom steam data
+        Steam.ready(function(err) {
+          if (err) return console.log(err);
+          var steam = new Steam();
+          steam.resolveVanityURL({vanityurl:custom_vanity}, function(err, data) {
+            // data -> { steamid: '76561197968620915', success: 1 }
+            json.social_handles.steam_id = data.steamid;
+            console.log(json.social_handles.steam_id);
+            var converted_json = JSON.stringify(json, null, 2); // spacing level = 2?
+            fs.writeFile('./config/config.json', converted_json, 'utf8'); // write it back
+        });
+      });
+    });
   // stupid name?
   // var display_name = steam.getPlayerSummaries({key:json.steam_api_key, steam_id});
   // console.log(display_name);
@@ -54,6 +47,10 @@ router.get("/",function(req,res){
   app.set('view engine', 'html');
   app.set('views','./views');
 
+  json = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+
+  console.log("this is after the new functions");
+  console.log(json.social_handles.steam_id);
   // display index.html
   res.render('index', { json: json});
 });
